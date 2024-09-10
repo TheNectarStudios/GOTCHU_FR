bda67aca0dcbfbff84298a9ea26f8d196760f4cc
nepla143 <nepalsss007@gmail.com>
2024-09-10 19:22:38 +0530

    only one player is working other is not

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -14,10 +14,10 @@ public class SpawnManager : MonoBehaviourPunCallbacks
     public GameObject protagonistPrefab;
     public GameObject antagonistPrefab;
 
-    public GameObject buttonUp;    // Reference to the UI buttons
-    public GameObject buttonDown;
-    public GameObject buttonLeft;
-    public GameObject buttonRight;
+    public Button buttonUp;    // Reference to the UI buttons
+    public Button buttonDown;
+    public Button buttonLeft;
+    public Button buttonRight;
 
     public float bufferTime = 3.0f;  // Time to wait before assigning roles
 
@@ -63,7 +63,7 @@ public class SpawnManager : MonoBehaviourPunCallbacks
         // Spawn protagonist at the designated spawn point
         GameObject protagonist = PhotonNetwork.Instantiate(protagonistPrefab.name, protagonistSpawnPoint.position, protagonistSpawnPoint.rotation);
 
-        // If this is the local player, link the movement to the UI buttons
+        // Assign UI button controls only for the local player
         if (photonView.IsMine)
         {
             AssignButtonControls(protagonist);
@@ -76,7 +76,7 @@ public class SpawnManager : MonoBehaviourPunCallbacks
         // Spawn antagonist at the designated spawn point
         GameObject antagonist = PhotonNetwork.Instantiate(antagonistPrefab.name, antagonistSpawnPoint.position, antagonistSpawnPoint.rotation);
 
-        // If this is the local player, link the movement to the UI buttons
+        // Assign UI button controls only for the local player
         if (photonView.IsMine)
         {
             AssignButtonControls(antagonist);
@@ -86,15 +86,26 @@ public class SpawnManager : MonoBehaviourPunCallbacks
     // Function to assign UI button controls to the local player
     private void AssignButtonControls(GameObject player)
     {
+        // Ensure the player object has the movement script
         PacMan3DMovement movementScript = player.GetComponent<PacMan3DMovement>();
 
         if (movementScript != null)
         {
+            // Remove existing listeners to avoid duplication
+            buttonUp.onClick.RemoveAllListeners();
+            buttonDown.onClick.RemoveAllListeners();
+            buttonLeft.onClick.RemoveAllListeners();
+            buttonRight.onClick.RemoveAllListeners();
+
             // Assign button actions
-            buttonUp.GetComponent<Button>().onClick.AddListener(movementScript.MoveUp);
-            buttonDown.GetComponent<Button>().onClick.AddListener(movementScript.MoveDown);
-            buttonLeft.GetComponent<Button>().onClick.AddListener(movementScript.MoveLeft);
-            buttonRight.GetComponent<Button>().onClick.AddListener(movementScript.MoveRight);
+            buttonUp.onClick.AddListener(() => movementScript.MoveUp());
+            buttonDown.onClick.AddListener(() => movementScript.MoveDown());
+            buttonLeft.onClick.AddListener(() => movementScript.MoveLeft());
+            buttonRight.onClick.AddListener(() => movementScript.MoveRight());
+        }
+        else
+        {
+            Debug.LogError("PacMan3DMovement script not found on the player.");
         }
     }
 }
