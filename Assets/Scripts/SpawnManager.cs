ac9eb10ad5f7d9caa221832940acf0f30a1d3a87
nepla143 <nepalsss007@gmail.com>
2024-09-10 19:30:11 +0530

    players are now working properly

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,39 +3,34 @@ using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;  // For UI Button
+using UnityEngine.UI;
 
 public class SpawnManager : MonoBehaviourPunCallbacks
 {
-    // Separate spawn points for protagonist and antagonist
     public Transform protagonistSpawnPoint;
     public Transform antagonistSpawnPoint;
 
     public GameObject protagonistPrefab;
     public GameObject antagonistPrefab;
 
-    public Button buttonUp;    // Reference to the UI buttons
+    public Button buttonUp;
     public Button buttonDown;
     public Button buttonLeft;
     public Button buttonRight;
 
-    public float bufferTime = 3.0f;  // Time to wait before assigning roles
+    public float bufferTime = 3.0f;
 
     private void Start()
     {
         if (PhotonNetwork.IsConnected && PhotonNetwork.InRoom && PhotonNetwork.IsMasterClient)
         {
-            // Start the role assignment after a buffer period
             StartCoroutine(WaitAndAssignRoles());
         }
     }
 
     private IEnumerator WaitAndAssignRoles()
     {
-        // Wait for the buffer time to allow all players to join
         yield return new WaitForSeconds(bufferTime);
-
-        // Now perform role assignment
         AssignRoles();
     }
 
@@ -60,52 +55,39 @@ public class SpawnManager : MonoBehaviourPunCallbacks
     [PunRPC]
     private void SetProtagonist()
     {
-        // Spawn protagonist at the designated spawn point
         GameObject protagonist = PhotonNetwork.Instantiate(protagonistPrefab.name, protagonistSpawnPoint.position, protagonistSpawnPoint.rotation);
-
-        // Assign UI button controls only for the local player
-        if (photonView.IsMine)
-        {
-            AssignButtonControls(protagonist);
-        }
+        AssignButtonControls(protagonist);
     }
 
     [PunRPC]
     private void SetAntagonist()
     {
-        // Spawn antagonist at the designated spawn point
         GameObject antagonist = PhotonNetwork.Instantiate(antagonistPrefab.name, antagonistSpawnPoint.position, antagonistSpawnPoint.rotation);
-
-        // Assign UI button controls only for the local player
-        if (photonView.IsMine)
-        {
-            AssignButtonControls(antagonist);
-        }
+        AssignButtonControls(antagonist);
     }
 
-    // Function to assign UI button controls to the local player
     private void AssignButtonControls(GameObject player)
     {
-        // Ensure the player object has the movement script
-        PacMan3DMovement movementScript = player.GetComponent<PacMan3DMovement>();
-
-        if (movementScript != null)
+        if (player.GetComponent<PhotonView>().IsMine)
         {
-            // Remove existing listeners to avoid duplication
-            buttonUp.onClick.RemoveAllListeners();
-            buttonDown.onClick.RemoveAllListeners();
-            buttonLeft.onClick.RemoveAllListeners();
-            buttonRight.onClick.RemoveAllListeners();
+            PacMan3DMovement movementScript = player.GetComponent<PacMan3DMovement>();
 
-            // Assign button actions
-            buttonUp.onClick.AddListener(() => movementScript.MoveUp());
-            buttonDown.onClick.AddListener(() => movementScript.MoveDown());
-            buttonLeft.onClick.AddListener(() => movementScript.MoveLeft());
-            buttonRight.onClick.AddListener(() => movementScript.MoveRight());
-        }
-        else
-        {
-            Debug.LogError("PacMan3DMovement script not found on the player.");
+            if (movementScript != null)
+            {
+                buttonUp.onClick.RemoveAllListeners();
+                buttonDown.onClick.RemoveAllListeners();
+                buttonLeft.onClick.RemoveAllListeners();
+                buttonRight.onClick.RemoveAllListeners();
+
+                buttonUp.onClick.AddListener(() => movementScript.MoveUp());
+                buttonDown.onClick.AddListener(() => movementScript.MoveDown());
+                buttonLeft.onClick.AddListener(() => movementScript.MoveLeft());
+                buttonRight.onClick.AddListener(() => movementScript.MoveRight());
+            }
+            else
+            {
+                Debug.LogError("PacMan3DMovement script not found on the player.");
+            }
         }
     }
 }
