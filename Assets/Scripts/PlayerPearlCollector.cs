f5b3e797c9d85aae469f4fc8885621b22f16f324
loyaldudee <mayurrishi2004gmail.com>
2024-09-17 23:10:10 +0530

    Pearl collection and bug fixed

--- Assets/Scripts/PlayerPearlCollector.cs
+++ Assets/Scripts/PlayerPearlCollector.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.SceneManagement;  // Required for scene management
 
 public class PlayerPearlCollector : MonoBehaviourPun
 {
     public int collectedPearls = 0;  // Track the number of collected pearls
+    private int totalPearlsToCollect;  // This will be set dynamically
+
+    private void Start()
+    {
+        // Find all GameObjects tagged as "Ghost" and set the totalPearlsToCollect based on that
+        GameObject[] ghosts = GameObject.FindGameObjectsWithTag("Ghost");
+        totalPearlsToCollect = ghosts.Length;  // Number of Ghosts equals the number of pearls to collect
+
+        Debug.Log("Total pearls to collect: " + totalPearlsToCollect);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -42,6 +53,13 @@ public class PlayerPearlCollector : MonoBehaviourPun
                 // If the player is the MasterClient, they can request ownership and destroy it
                 RequestOwnershipAndDestroy(pearlView);
             }
+        }
+
+        // Check if collected pearls reach the totalPearlsToCollect and then change the scene
+        if (collectedPearls >= totalPearlsToCollect)
+        {
+            Debug.Log("Collected all pearls! Switching to RoomCreated scene.");
+            photonView.RPC("ChangeSceneForAllPlayers", RpcTarget.All);
         }
     }
 
@@ -80,5 +98,13 @@ public class PlayerPearlCollector : MonoBehaviourPun
         {
             Debug.LogError("Failed to take ownership of the pearl. Unable to destroy.");
         }
+    }
+
+    [PunRPC]
+    void ChangeSceneForAllPlayers()
+    {
+        // Load the RoomCreated scene for all players in the room
+        Debug.Log("Loading RoomCreated scene for all players...");
+        PhotonNetwork.LoadLevel("RoomCreated");
     }
 }
