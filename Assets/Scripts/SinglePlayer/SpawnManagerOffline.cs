74ee26505af61b29f6ec4cc82f4bfd812177699b
loyaldudee <mayurrishi2004gmail.com>
2024-10-25 00:45:29 +0530

    do not click on single player yet

--- Assets/Scripts/SinglePlayer/SpawnManagerOffline.cs
+++ Assets/Scripts/SinglePlayer/SpawnManagerOffline.cs
@@ -6,135 +6,108 @@ using UnityEngine.UI;
 public class SpawnManagerOffline : MonoBehaviour
 {
     public Transform protagonistSpawnPoint;
-    public Material freezeEffectMaterial;
+    public GameObject protagonistPrefab;
 
     public Transform[] antagonistSpawnPoints;
-    public Button powerButton;
-    private string currentPowerUp = null;
-
-    public GameObject protagonistPrefab;
     public GameObject antagonistPrefab;
-    public GameObject bulletPrefab;
 
-    public float bufferTime = 3.0f;
-    private bool isCooldown = false;
+    public GameObject joystickPrefab;
+    public GameObject controlUI;
+    public GameObject loadingScreen;
+    public GameObject timerObject;
 
     public Image powerUpThumbnail;
     public Sprite freezeSprite;
     public Sprite bulletSprite;
     public Sprite speedBoostSprite;
 
-    public GameObject protagonistPanel;
-    public GameObject antagonistPanel;  // Single antagonist panel
-
-    public GameObject controlUI;
-    public GameObject loadingScreen;
-
-    public GameObject timerObject;
-
-    public GameObject joystickPrefab;
-
-    public GameObject antagonistInvisibilityPanel;
-    public GameObject antagonistDashPanel;
-    public GameObject antagonistTrapPanel;
-
-    private ShaderManager shaderManager;
-    private int spawnedGhostsCount = 0;  // Keep track of ghosts spawned
+    private int spawnedGhostsCount = 0;
+    private string currentPowerUp = null;
 
     private void Start()
     {
-        shaderManager = FindObjectOfType<ShaderManager>();
-        StartCoroutine(WaitAndAssignRoles());
+        StartCoroutine(SpawnEntitiesAfterDelay());
     }
 
-    private IEnumerator WaitAndAssignRoles()
+    private IEnumerator SpawnEntitiesAfterDelay()
     {
-        yield return new WaitForSeconds(bufferTime);
+        yield return new WaitForSeconds(3.0f); // Optional delay before spawning
 
         HideLoadingScreen();
-        HideAllRolePanels();
 
-        AssignRoles();
+        // Spawn protagonist
+        SpawnProtagonist();
 
-        yield return new WaitForSeconds(3f);
-        HideAllRolePanels();
-        ShowControlUI();
+        // Spawn antagonists
+        SpawnAntagonists();
+
         yield return new WaitForSeconds(2f);
+
+        ShowControlUI();
         StartTimer();
     }
 
-    private void AssignRoles()
+    private void SpawnProtagonist()
     {
-        List<int> players = new List<int> { 0, 1, 2, 3 };  // Dummy list representing 4 players
-        int protagonistIndex = Random.Range(0, players.Count);
-
-        for (int i = 0; i < players.Count; i++)
+        if (protagonistPrefab != null && protagonistSpawnPoint != null)
         {
-            if (i == protagonistIndex)
-            {
-                SetProtagonist();
-            }
-            else
-            {
-                SetAntagonist();
-            }
+            GameObject protagonist = Instantiate(protagonistPrefab, protagonistSpawnPoint.position, protagonistSpawnPoint.rotation);
+            EnableJoystick(); // Enable joystick for protagonist
         }
     }
 
-    private void SetProtagonist()
+    private void SpawnAntagonists()
     {
-        GameObject protagonist = Instantiate(protagonistPrefab, protagonistSpawnPoint.position, protagonistSpawnPoint.rotation);
-        AssignButtonControls(protagonist);
-        AssignCamera(protagonist);
-        ShowPanel(protagonistPanel);
-        EnableJoystick(); // Enable the joystick for the protagonist
+        for (int i = 0; i < antagonistSpawnPoints.Length; i++)
+        {
+            if (spawnedGhostsCount < antagonistSpawnPoints.Length)
+            {
+                Transform spawnPoint = antagonistSpawnPoints[spawnedGhostsCount];
+                Instantiate(antagonistPrefab, spawnPoint.position, spawnPoint.rotation);
+                spawnedGhostsCount++;
+            }
+        }
+
+        EnableJoystick(); // Enable joystick for antagonists as well, if required
     }
 
-    private void SetAntagonist()
+    private void EnableJoystick()
     {
-        if (spawnedGhostsCount < antagonistSpawnPoints.Length)
+        if (joystickPrefab != null)
         {
-            Transform spawnPoint = antagonistSpawnPoints[spawnedGhostsCount];
-            GameObject antagonist = Instantiate(antagonistPrefab, spawnPoint.position, spawnPoint.rotation);
-            AssignButtonControls(antagonist);
-            AssignCamera(antagonist);
-
-            AssignAbilities(antagonist);
-            EnableJoystick(); // Enable the joystick for the antagonist
-            spawnedGhostsCount++;
+            joystickPrefab.SetActive(true);
         }
     }
 
-    private void AssignButtonControls(GameObject player)
+    private void ShowControlUI()
     {
-        PacMan3DMovement movementScript = player.GetComponent<PacMan3DMovement>();
-        if (movementScript != null)
+        if (controlUI != null)
         {
-            powerButton.interactable = true;
-            powerButton.onClick.RemoveAllListeners();
-            powerButton.onClick.AddListener(ActivateStoredPowerUp);
+            controlUI.SetActive(true);
         }
     }
 
-    private void ActivateStoredPowerUp()
+    private void HideLoadingScreen()
     {
-        if (currentPowerUp != null)
+        if (loadingScreen != null)
         {
-            if (currentPowerUp == "Freeze")
-            {
-                ActivateFreezePowerUp();
-            }
-            else if (currentPowerUp == "Bullet")
-            {
-                ActivateBulletPowerUp();
-            }
-            else if (currentPowerUp == "SpeedBoost")
-            {
-                ActivateSpeedBoostPowerUp();
-            }
-            currentPowerUp = null;
-            HidePowerUpThumbnail();
+            loadingScreen.SetActive(false);
         }
+    }
+
+    private void StartTimer()
+    {
+        if (timerObject != null)
+        {
+            timerObject.SetActive(true);
+        }
+    }
+
+    // Power-up management
+    public void UpdateInventory(string powerUpName)
+    {
+        currentPowerUp = powerUpName;
+        ShowPowerUpThumbnail(GetPowerUpSprite(powerUpName));
     }
 
     private void ShowPowerUpThumbnail(Sprite powerUpSprite)
@@ -154,73 +127,6 @@ public class SpawnManagerOffline : MonoBehaviour
         }
     }
 
-    private void AssignCamera(GameObject player)
-    {
-        Camera mainCamera = Camera.main;
-
-        if (mainCamera != null)
-        {
-            TopDownCameraFollow cameraFollowScript = mainCamera.GetComponent<TopDownCameraFollow>();
-
-            if (cameraFollowScript == null)
-            {
-                cameraFollowScript = mainCamera.gameObject.AddComponent<TopDownCameraFollow>();
-            }
-
-            cameraFollowScript.target = player.transform;
-        }
-    }
-
-    private void AssignAbilities(GameObject player)
-    {
-        powerButton.interactable = true;
-        powerButton.onClick.RemoveAllListeners();
-
-        Invisibility invisibility = player.GetComponent<Invisibility>();
-        Dash dash = player.GetComponent<Dash>();
-        Trap trap = player.GetComponent<Trap>();
-
-        if (invisibility != null)
-        {
-            powerButton.onClick.AddListener(() => ActivatePower(invisibility.ActivateInvisibility, invisibility.cooldownTime));
-            ShowPanel(antagonistInvisibilityPanel);
-        }
-        if (dash != null)
-        {
-            powerButton.onClick.AddListener(() => ActivatePower(dash.ActivateDash, dash.cooldownTime));
-            ShowPanel(antagonistDashPanel);
-        }
-        if (trap != null)
-        {
-            powerButton.onClick.AddListener(() => ActivatePower(trap.PlaceTrap, trap.cooldownTime));
-            ShowPanel(antagonistTrapPanel);
-        }
-    }
-
-    private void ActivatePower(System.Action powerAction, float cooldown)
-    {
-        if (!isCooldown)
-        {
-            powerAction.Invoke();
-            StartCoroutine(CooldownRoutine(cooldown));
-        }
-    }
-
-    private IEnumerator CooldownRoutine(float cooldown)
-    {
-        isCooldown = true;
-        powerButton.interactable = false;
-        yield return new WaitForSeconds(cooldown);
-        powerButton.interactable = true;
-        isCooldown = false;
-    }
-
-    public void UpdateInventory(string powerUpName)
-    {
-        currentPowerUp = powerUpName;
-        ShowPowerUpThumbnail(GetPowerUpSprite(powerUpName));
-    }
-
     private Sprite GetPowerUpSprite(string powerUpName)
     {
         switch (powerUpName)
@@ -229,150 +135,6 @@ public class SpawnManagerOffline : MonoBehaviour
             case "Bullet": return bulletSprite;
             case "SpeedBoost": return speedBoostSprite;
             default: return null;
-        }
-    }
-
-    private void ActivateFreezePowerUp()
-    {
-        FreezeGhosts();
-    }
-
-    private void FreezeGhosts()
-    {
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Ghost");
-        foreach (GameObject enemy in enemies)
-        {
-            PacMan3DMovement enemyMovement = enemy.GetComponent<PacMan3DMovement>();
-            if (enemyMovement != null)
-            {
-                enemyMovement.enabled = false;
-                Rigidbody ghostRigidbody = enemy.GetComponent<Rigidbody>();
-                if (ghostRigidbody != null)
-                {
-                    ghostRigidbody.velocity = Vector3.zero;
-                    ghostRigidbody.angularVelocity = Vector3.zero;
-                }
-
-                Animator ghostAnimator = enemy.GetComponent<Animator>();
-                if (ghostAnimator != null)
-                {
-                    ghostAnimator.enabled = false;
-                }
-
-                if (freezeEffectMaterial != null)
-                {
-                    // Apply freeze effect material or any other visual effect
-                }
-
-                StartCoroutine(ReEnableMovement(enemyMovement, 5f));
-            }
-        }
-    }
-
-    private IEnumerator ReEnableMovement(PacMan3DMovement enemyMovement, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-
-        if (enemyMovement != null)
-        {
-            enemyMovement.enabled = true;
-        }
-
-        Animator ghostAnimator = enemyMovement.GetComponent<Animator>();
-        if (ghostAnimator != null)
-        {
-            ghostAnimator.enabled = true;
-        }
-
-        if (freezeEffectMaterial != null)
-        {
-            // Remove freeze effect material or reset visual effect
-        }
-    }
-
-    private void ActivateBulletPowerUp()
-    {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-
-        if (player != null)
-        {
-            FireBullet(player.transform);
-        }
-    }
-
-    private void FireBullet(Transform playerTransform)
-    {
-        if (bulletPrefab != null)
-        {
-            GameObject bullet = Instantiate(bulletPrefab, playerTransform.position, playerTransform.rotation);
-            Rigidbody rb = bullet.GetComponent<Rigidbody>();
-            rb.velocity = playerTransform.forward * 10f;  // Set bullet speed
-        }
-    }
-
-    private void ActivateSpeedBoostPowerUp()
-    {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-
-        if (player != null)
-        {
-            PacMan3DMovement movementScript = player.GetComponent<PacMan3DMovement>();
-
-            if (movementScript != null)
-            {
-                movementScript.speed += 2.0f;
-                StartCoroutine(ResetSpeedAfterDelay(movementScript, 5f));
-            }
-        }
-    }
-
-    private IEnumerator ResetSpeedAfterDelay(PacMan3DMovement movementScript, float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        movementScript.speed -= 2.0f;
-    }
-
-    private void ShowLoadingScreen()
-    {
-        loadingScreen.SetActive(true);
-    }
-
-    private void HideLoadingScreen()
-    {
-        loadingScreen.SetActive(false);
-    }
-
-    private void ShowControlUI()
-    {
-        controlUI.SetActive(true);
-    }
-
-    private void HideAllRolePanels()
-    {
-        protagonistPanel.SetActive(false);
-        antagonistInvisibilityPanel.SetActive(false);
-        antagonistDashPanel.SetActive(false);
-        antagonistTrapPanel.SetActive(false);
-    }
-
-    private void ShowPanel(GameObject panel)
-    {
-        panel.SetActive(true);
-    }
-
-    private void EnableJoystick()
-    {
-        if (joystickPrefab != null)
-        {
-            joystickPrefab.SetActive(true);
-        }
-    }
-
-    private void StartTimer()
-    {
-        if (timerObject != null)
-        {
-            timerObject.SetActive(true);
         }
     }
 }
